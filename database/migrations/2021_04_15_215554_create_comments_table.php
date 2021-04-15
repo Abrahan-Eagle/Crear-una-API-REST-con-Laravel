4b34f66cf83356b76434c5b2b852207934da78b2
Abrahan Pulido <thehackeroffire@gmail.com>
2021-04-15 18:13:18 -0400

    #12 #11 Relaciones entre entidades - Crear Modelo Comments

--- database/migrations/2021_04_15_215554_create_comments_table.php
+++ database/migrations/2021_04_15_215554_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateArticlesTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,19 @@ class CreateArticlesTable extends Migration
      */
     public function up()
     {
-        Schema::create('articles', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
+
             $table->id();
 
+            $table->unsignedBigInteger('article_id')->nullable();
             $table->unsignedBigInteger('user_id')->nullable();
 
-            $table->string('title');
-            $table->text('body');
+            $table->text('text');
 
             $table->timestamps();
 
-            //RELACIONES
+            //RELACIONES BBDD
+            $table->foreign('article_id')->references('id')->on('articles')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
 
         });
@@ -36,6 +38,6 @@ class CreateArticlesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('articles');
+        Schema::dropIfExists('comments');
     }
 }
