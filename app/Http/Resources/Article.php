63e7561f5bda35d52f9b343c880e7bf8bf3db7c4
Abrahan Pulido <thehackeroffire@gmail.com>
2021-04-18 18:49:03 -0400

    #14 recursos apis

---
+++ app/Http/Resources/Article.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Article extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {   //return parent::toArray($request);
+        return [
+            'id'         => $this->id,
+            'title'      => $this->title,
+            'body'       => $this->body,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            //-------------------------------------------------
+            //'user_id'    => $this->user_id,
+            //'category_id'=> $this->category_id,
+            //-------------------------------------------------
+            //'user'    => User::find($this->user_id),
+            //'category'=> Category::find($this->category_id),
+            //-------------------------------------------------
+            'user'    => '/api/users/' . $this->user_id,
+            'category'=> '/api/category/' . $this->category_id,
+        ];
+    }
+}
